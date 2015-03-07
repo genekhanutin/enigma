7ff971a3bf9da60f619404c084414e4f136cfb89
genekhanutin <genekhanutin@gmail.com>
2015-03-07 11:50:21 -0700

    encrypts and decrypts messages

--- tests/rotator_test.rb
+++ tests/rotator_test.rb
@@ -33,103 +33,134 @@ class RotatorTest < Minitest::Test
 		assert_equal 60, d 
 	end
 
-	def test_rotation_a_rotates_first_letter_by_first_rotation_value
+	def test_rotation_a_rotates_first_character_by_first_rotation_value
 		rotator = Rotator.new(23456, 1234, "jklm")
 		rotation_values = rotator.rotation_value_generator
 
 		assert_equal "7", rotator.rotation_a(rotation_values)
 	end
 
-	def test_rotation_b_rotates_second_letter_by_second_rotation_value
+	def test_rotation_b_rotates_second_character_by_second_rotation_value
 		rotator = Rotator.new(23456, 1234, "abcd")
 		rotation_values = rotator.rotation_value_generator
 
 		assert_equal ".", rotator.rotation_b(rotation_values)
 	end
 
-	def test_rotation_c_rotates_third_letter_by_third_rotation_value
+	def test_rotation_c_rotates_third_character_by_third_rotation_value
 		rotator = Rotator.new(23456, 1234, "abcd")
 
 		assert_equal "l", rotator.rotation_c(rotator.rotation_value_generator)
 	end
 
-	def test_it_rotates_four_characters_by_total_rotation_valuesd
+	def test_it_rotates_fourth_character_by_total_rotation_values
 		rotator = Rotator.new(23456, 1234, "abcd")
 
 		assert_equal "y", rotator.rotation_d(rotator.rotation_value_generator)
 	end
 
 	def test_it_encrypts_four_characters_at_a_time
-		rotator = Rotator.new(23456, 1234, "jbcd")
-
-		assert_equal "7.ly", rotator.encrypt
+		assert_equal "7.ly", Rotator.encrypt(23456, 1234, "jbcd")
   end
 
 	def test_it_can_encrypt_eight_letters
-		# taking stock - what do we have
-		# we have a Rotator object, which given a string
-		# will encrypt the first 4 characters of that string
-		# and return them
-		message = "jbcdfefe"
-		key = 23456
+		message     = "jbcdfefe"
+		key         = 23456
 		date_offset = 1234
 
-		# [["j","b","c","d"],["f","e","f","e"]]
-		
-		#encrypted: Array
-		# of: 4-character Strings
-		# where each string is encrypted
-		# --> array of my encrypted message in
-		# 4-character chunks
-
-			# why is this passing?
 		assert_equal "7.ly3boz", Rotator.encrypt(key, date_offset, message)
 	end
 
 		def test_it_can_encrypt_seven_letters
-		message = "jbcdfef"
-		key = 23456
+		message     = "jbcdfef"
+		key         = 23456
 		date_offset = 1234
 
 		assert_equal "7.ly3bo", Rotator.encrypt(key, date_offset, message)
 	end
 
 	def test_it_can_encrypt_six_letters
-		message = "jbcdfe"
-		key = 23456
+		message     = "jbcdfe"
+		key         = 23456
 		date_offset = 1234
 
 		assert_equal "7.ly3b", Rotator.encrypt(key, date_offset, message)
 	end
 
 	def test_it_can_encrypt_five_letters
-		message = "jbcdf"
-		key = 23456
+		message     = "jbcdf"
+		key         = 23456
 		date_offset = 1234
 
 		assert_equal "7.ly3", Rotator.encrypt(key, date_offset, message)
 	end
 
 	def test_it_can_encrypt_three_letters
-		message = "jbc"
-		key = 23456
+		message     = "jbc"
+		key         = 23456
 		date_offset = 1234
 
 		assert_equal "7.l", Rotator.encrypt(key, date_offset, message)
 	end
 
 	def test_it_can_encrypt_no_letters
-		message = ""
-		key = 23456
+		message     = ""
+		key         = 23456
 		date_offset = 1234
 
 		assert_equal "", Rotator.encrypt(key, date_offset, message)
 	end
 
-
 	def test_it_decrypts_four_characters_at_a_time
-		rotator = Rotator.new(23456, 1234, "7.ly")
+		message 		= "7.ly"
+		key 				= 23456
+		date_offset = 1234
 
-		assert_equal "jbcd", rotator.decrypt
+		assert_equal "jbcd", Rotator.decrypt(key, date_offset, message)
+  end
+
+  def test_it_decrypts_five_characters_at_a_time
+  	message 		= "7.ly3"
+  	key     		= 23456
+  	date_offset = 1234
+
+		assert_equal "jbcdf", Rotator.decrypt(key, date_offset, message)
+	end
+
+	def test_it_decrypts_six_characters_at_a_time
+  	message 		= "7.ly3b"
+  	key     		= 23456
+  	date_offset = 1234
+  	
+		assert_equal "jbcdfe", Rotator.decrypt(key, date_offset, message)
+	end
+
+	def test_it_decrypts_five_characters_at_a_time
+  	message 		= "7.ly3"
+  	key     		= 23456
+  	date_offset = 1234
+  	
+		assert_equal "jbcdf", Rotator.decrypt(key, date_offset, message)
+	end
+
+	def test_it_decrypts_three_characters_at_a_time
+		message 		= "7.l"
+  	key     		= 23456
+  	date_offset = 1234
+
+  	assert_equal "jbc", Rotator.decrypt(key, date_offset, message)
+  end
+
+  def test_it_decrypts_one_character
+  	message 		= "7"
+  	key     		= 23456
+  	date_offset = 1234
+
+  	assert_equal "j", Rotator.decrypt(key, date_offset, message)
   end
 end
+
+
+
+
+
